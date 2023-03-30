47b0d09afb61977ce1c429a703f6576b521ee901
Facundo Masino <facujmasino@gmail.com>
2023-03-30 11:09:38 -0300

    Create ejercicio1tp1.cpp

---
+++ LaboratorioI/TP1/ejercicio1tp1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+/*
+Hacer un programa para ingresar por teclado la cantidad de horas 
+trabajadas por un operario y el valor que se le paga por hora 
+trabajada y listar por pantalla el sueldo que le corresponda.
+*/
+
+int main() {
+  int horas, valorHora, salir;
+
+  cout << "Ingrese la cantidad de horas trabajadas:";
+  cin >> horas;
+  cout << "Ingrese el valor cobrado por hora:";
+  cin >> valorHora;
+  cout << endl << "El sueldo es de: $" << horas*valorHora << endl;
+  cout << endl;
+  system("pause");
+
+  return 0;
+}
