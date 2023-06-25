a505935c81062e1851e060d85c66a492d2db3d7e
Facundo Masino <facujmasino@gmail.com>
2023-06-24 22:39:43 -0300

    Update main.cpp

--- LaboratorioI/TP6/TP6E5/main.cpp
+++ LaboratorioI/TP6/TP6E5/main.cpp
@@ -1,3 +1,10 @@
+/*
+Cargar un conjunto de 100 números reales y formar con los valores positivos 
+un vector llamado pos y con los valores negativos un vector llamado nega. 
+Mostrar por pantalla la cantidad de elementos que contiene cada vector y luego 
+cada uno de los vectores. Sólo deben utilizarse dos vectores en el programa: 
+pos y nega.
+*/
 #include <iostream>
 using namespace std;
 
