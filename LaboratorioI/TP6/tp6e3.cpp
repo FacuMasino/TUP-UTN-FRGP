0a53bd0907e03514389cf715ebcefd8d7ce74815
Facundo Masino <facujmasino@gmail.com>
2023-06-24 22:35:41 -0300

    Update tp6e3.cpp

--- LaboratorioI/TP6/tp6e3.cpp
+++ LaboratorioI/TP6/tp6e3.cpp
@@ -1,7 +1,12 @@
+/*
+Hacer un programa donde se carguen 15 números enteros y luego 
+muestre por pantalla el mínimo elemento del vector y además 
+indique cuántas veces se repite el valor mínimo dentro del vector.
+*/
 #include <iostream>
 using namespace std;
 int main(void) {
-  const int TOTAL = 3;
+  const int TOTAL = 15;
   int numeros[TOTAL] {};
   int menor;
   int repetido = 0;
